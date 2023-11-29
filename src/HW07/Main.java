5ffa775c07579127ebf0db6d335d5773700ea42b
Dmytro Halytskyi <ds.galytskyi@gmail.com>
2023-11-29 22:10:57 +0200

    add solution with for-each

--- src/HW07/Main.java
+++ src/HW07/Main.java
@@ -1,7 +1,6 @@
 package HW07;
 
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
 
@@ -13,14 +12,23 @@ public class Main {
 
         ticketNumbers = new int[][]{{23, 30, 89, 42}, {54, 68, 43, 11}, {90, 32, 44, 55}, {78, 52, 63, 31}};
 
+        System.out.println("Lottery ticket numbers: ");
+
+        for (int i = 0; i < ticketNumbers.length; i += 1) {
+            for (int j = 0; j < ticketNumbers[i].length; j += 1) {
+                System.out.print(ticketNumbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+
         System.out.print("\nPlease, enter a number: ");
         Scanner ticketScanner = new Scanner(System.in);
         searchNum = ticketScanner.nextInt();
         ticketScanner.nextLine();
 
         int index = 0;
-        for (int i = 0; i < ticketNumbers.length; i += 1) {
-            for (int j = 0; j < ticketNumbers[i].length; j += 1) {
+        for (int[] ticketNumber : ticketNumbers) {
+            for (int oneNum : ticketNumber) {
                 if (searchNum != WINTICKET) {
                     index += 1;
                 }
